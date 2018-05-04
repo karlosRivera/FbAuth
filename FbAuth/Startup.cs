9976b82bc0700d294668e550357834d54119e72b
tridip-bba <tridip@bba-reman.com>
2018-05-05 01:25:43 +0530

    Initial commit

---
+++ FbAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FbAuth.Startup))]
+namespace FbAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
